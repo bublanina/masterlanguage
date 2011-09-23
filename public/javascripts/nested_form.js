6e0e1ea602f3b8bbce3fe5bd3b0be288314a51cb
bublanina <ivazuz@gmail.com>
2011-09-23 09:00:25 +0200

    first

--- public/javascripts/nested_form.js
+++ public/javascripts/nested_form.js
@@ -9,27 +9,30 @@ document.observe('click', function(e, el) {
 	  var context = (el.getOffsetParent('.fields').firstDescendant().readAttribute('name') || '').replace(new RegExp('\[[a-z]+\]$'), '');
 
 	  // context will be something like this for a brand new form:
-	  // project[tasks_attributes][1255929127459][assignments_attributes][1255929128105]
+	  // project[tasks_attributes][new_1255929127459][assignments_attributes][new_1255929128105]
 	  // or for an edit form:
 	  // project[tasks_attributes][0][assignments_attributes][1]
 	  if(context) {
 	    var parent_names = context.match(/[a-z_]+_attributes/g) || [];
-	    var parent_ids   = context.match(/[0-9]+/g);
+	    var parent_ids   = context.match(/(new_)?[0-9]+/g) || [];
 
 	    for(i = 0; i < parent_names.length; i++) {
-	      if(parent_ids[i]) {
-	        content = content.replace(
-	          new RegExp('(\\[' + parent_names[i] + '\\])\\[.+?\\]', 'g'),
-	          '$1[' + parent_ids[i] + ']'
-	        )
-	      }
+        if(parent_ids[i]) {
+          content = content.replace(
+            new RegExp('(_' + parent_names[i] + ')_.+?_', 'g'),
+            '$1_' + parent_ids[i] + '_');
+
+          content = content.replace(
+            new RegExp('(\\[' + parent_names[i] + '\\])\\[.+?\\]', 'g'),
+            '$1[' + parent_ids[i] + ']');
+        }
 	    }
 	  }
 
 	  // Make a unique ID for the new child
 	  var regexp  = new RegExp('new_' + assoc, 'g');
 	  var new_id  = new Date().getTime();
-	  content     = content.replace(regexp, new_id);
+	  content     = content.replace(regexp, "new_" + new_id);
 
 	  el.insert({ before: content });
 	  return false;
